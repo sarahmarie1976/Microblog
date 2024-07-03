d5a12a5b99e99d051d0816a5c8a482d60115aae2
Bryan Jones <bryanjones@Bryans-MBP.attlocal.net>
2024-07-03 18:27:58 -0500

    :page_facing_up: added templates base and index

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app 
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello Nala!"
+    user = {'username': 'Nala'}
+    posts = [
+        {
+            'author': {'username': 'Jonesma'},
+            'body': 'Beautiful day in Missouri!'
+        },
+        {
+            'author': {'username': 'Walter'},
+            'body': 'Nightmare before Christmas was great!'
+        },
+        
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    
