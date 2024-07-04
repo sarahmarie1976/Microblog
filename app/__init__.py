3b84c838630fc38ad5e17425bd45b9be2c5220b6
Bryan Jones <bryanjones@Bryans-MBP.attlocal.net>
2024-07-03 19:48:15 -0500

    (:page_facing_up updated files and added forms, login and config files

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 CORS(app)
 
 from app import routes
