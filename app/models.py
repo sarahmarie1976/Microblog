dae99749628986dee3b629c8a5968e9192d51aa5
Bryan Jones <bryanjones@Bryans-MBP.attlocal.net>
2024-07-04 19:04:19 -0500

    :page_facing_up: created register user and updated login, forms, and routes

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
+from flask_login import UserMixin
 from app import db
 
-class User(db.Model):
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -24,3 +27,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
